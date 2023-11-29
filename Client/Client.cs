2bf64b2dd44e8e2563e0fab64d43976cda7b985c
ArtemBrigidin <artemgithuub@gmail.com>
2023-11-29 14:53:37 +0300

    Добавьте файлы проекта.

---
+++ Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 8888);
+
+        using (StreamReader reader = new StreamReader(client.GetStream()))
+        using (StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true })
+        {
+            string welcomeMessage = reader.ReadLine();
+            Console.WriteLine(welcomeMessage);
+
+            int victories = 0;
+
+            while (victories != 5)
+            {
+                string result = reader.ReadLine();
+                Console.WriteLine(result);
+
+                if (result.Contains("Победа") || result.Contains("Поражение"))
+                {
+                    victories++;
+                }
+                if (victories == 5)
+                {
+                    Console.WriteLine("Пять побед! Игра завершена.");
+                    break;
+                }
+
+                string message = reader.ReadLine();
+                Console.WriteLine(message);
+
+                Console.Write("Ваш выбор: ");
+                int choice = int.Parse(Console.ReadLine());
+                writer.WriteLine(choice);
+
+                string score = reader.ReadLine();
+                Console.WriteLine(score);
+            }
+
+            Console.WriteLine("До встречи!");
+
+        }
+
+        client.Close();
+    }
+}
---
+++ RockPaperScissors-TCP/Server.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 8888);
+        server.Start();
+        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+        while (true)
+        {
+            TcpClient client1 = server.AcceptTcpClient();
+            TcpClient client2 = server.AcceptTcpClient();
+
+            ServerObject serverObject = new ServerObject(client1, client2);
+            Thread clientThread = new Thread(serverObject.Process);
+            clientThread.Start();
+        }
+    }
+}
+
+class ServerObject
+{
+    private TcpClient client1;
+    private TcpClient client2;
+
+    public ServerObject(TcpClient client1, TcpClient client2)
+    {
+        this.client1 = client1;
+        this.client2 = client2;
+    }
+
+    public void Process()
+    {
+        try
+        {
+            using (StreamReader reader1 = new StreamReader(client1.GetStream()))
+            using (StreamWriter writer1 = new StreamWriter(client1.GetStream()) { AutoFlush = true })
+            using (StreamReader reader2 = new StreamReader(client2.GetStream()))
+            using (StreamWriter writer2 = new StreamWriter(client2.GetStream()) { AutoFlush = true })
+            {
+                SendWelcomeMessage(writer1, "Выберите: 1 - Камень, 2 - Ножницы, 3 - Бумага");
+                SendWelcomeMessage(writer2, "Выберите: 1 - Камень, 2 - Ножницы, 3 - Бумага");
+
+                int score1 = 0;
+                int score2 = 0;
+
+                while (score1 < 5 && score2 < 5)
+                {
+                    int choice1 = int.Parse(ReceiveChoice(reader1, writer1));
+                    int choice2 = int.Parse(ReceiveChoice(reader2, writer2));
+
+                    var updatedScores = UpdateScores(choice1, choice2, score1, score2, writer1, writer2);
+                    score1 = updatedScores.Item1;
+                    score2 = updatedScores.Item2;
+                }
+
+                SendResult(score1, score2, writer1, writer2);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            client1.Close();
+            client2.Close();
+        }
+    }
+
+    private void SendWelcomeMessage(StreamWriter writer, string message)
+    {
+        writer.WriteLine("Добро пожаловать в игру Камень, Ножницы, Бумага!");
+        writer.WriteLine(message);
+    }
+
+    private string ReceiveChoice(StreamReader reader, StreamWriter writer)
+    {
+        writer.WriteLine("Ваш ход:");
+        return reader.ReadLine();
+    }
+
+    private (int score1, int score2) UpdateScores(int choice1, int choice2, int score1, int score2, StreamWriter writer1, StreamWriter writer2)
+    {
+        int updatedScore1 = score1;
+        int updatedScore2 = score2;
+
+        if ((choice1 == 1 && choice2 == 2) || (choice1 == 2 && choice2 == 3) || (choice1 == 3 && choice2 == 1))
+        {
+            updatedScore1++;
+            SendRoundResult(writer1, writer2, "Победа", "Поражение");
+        }
+        else if ((choice2 == 1 && choice1 == 2) || (choice2 == 2 && choice1 == 3) || (choice2 == 3 && choice1 == 1))
+        {
+            updatedScore2++;
+            SendRoundResult(writer2, writer1, "Победа", "Поражение");
+        }
+        else
+        {
+            SendRoundResult(writer1, writer2, "Ничья", "Ничья");
+        }
+
+        SendScores(writer1, writer2, updatedScore1, updatedScore2);
+
+        return (updatedScore1, updatedScore2);
+    }
+
+
+    private void SendRoundResult(StreamWriter winner, StreamWriter loser, string winMessage, string loseMessage)
+    {
+        winner.WriteLine(winMessage);
+        loser.WriteLine(loseMessage);
+    }
+
+    private void SendScores(StreamWriter writer1, StreamWriter writer2, int score1, int score2)
+    {
+        writer1.WriteLine($"Счет: {score1} - {score2}");
+        writer2.WriteLine($"Счет: {score2} - {score1}");
+    }
+
+    private void SendResult(int score1, int score2, StreamWriter writer1, StreamWriter writer2)
+    {
+        if (score1 == 5)
+        {
+            SendGameResult(writer1, writer2, "Победа!", "Поражение.");
+        }
+        else
+        {
+            SendGameResult(writer2, writer1, "Победа!", "Поражение.");
+        }
+    }
+
+    private void SendGameResult(StreamWriter winner, StreamWriter loser, string winMessage, string loseMessage)
+    {
+        winner.WriteLine(winMessage);
+        loser.WriteLine(loseMessage);
+    }
+}
